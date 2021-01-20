a56172599a29e41aa8dcf513d3ce7875044175ea
RodolfoAlvarenga <rodolfo.l.alvarenga@gmail.com>
2021-01-20 12:18:45 -0300

    GET by id for clients and exception handler

--- backend/src/main/java/com/devsuperior/clientapi/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/clientapi/services/ClientService.java
@@ -1,5 +1,7 @@
 package com.devsuperior.clientapi.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -9,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.clientapi.dto.ClientDTO;
 import com.devsuperior.clientapi.entities.Client;
 import com.devsuperior.clientapi.repositories.ClientRepository;
+import com.devsuperior.clientapi.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -20,5 +23,12 @@ public class ClientService {
 	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
 		Page<Client> list = repository.findAll(pageRequest);
 		return list.map(x -> new ClientDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new ClientDTO(entity);
 	}
 }
